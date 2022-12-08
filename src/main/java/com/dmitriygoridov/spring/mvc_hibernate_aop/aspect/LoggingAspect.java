49853ac8a6f99718a1ef763fafee94a60b7555d1
d.goridov <dimagoridov@yandex.ru>
2022-12-08 22:39:50 +0300

    Настройка сквозного логгирования в консоль и файл

--- src/main/java/com/dmitriygoridov/spring/mvc_hibernate_aop/aspect/LoggingAspect.java
+++ src/main/java/com/dmitriygoridov/spring/mvc_hibernate_aop/aspect/LoggingAspect.java
@@ -4,21 +4,24 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.reflect.MethodSignature;
+import org.slf4j.*;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
 public class LoggingAspect {
 
+    private static final Logger LOG = LoggerFactory.getLogger(LoggingAspect.class);
+
     @Around("execution(* com.dmitriygoridov.spring.mvc_hibernate_aop.dao.*.*(..))")
     public Object aroudAllRepositoryMethodsAdvice(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
         MethodSignature methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
         String methodName = methodSignature.getName();
-        System.out.println("Begin of " + methodName);
+        LOG.info("Begin of " + methodName);
 
         Object targetMethodResult = proceedingJoinPoint.proceed();
 
-        System.out.println("End of " + methodName);
+        LOG.info("End of " + methodName);
 
         return targetMethodResult;
     }
